12060835d5545d984a3ad265105932a9298d19a5
Philipp Batroff <batroff@systopia.de>
2019-08-09 14:10:33 +0200

    Fix special characters ni spell description causing pdflatex errros, creating errors

--- spells/spells_d.py
+++ spells/spells_d.py
@@ -376,8 +376,8 @@ class DimensionDoor(Spell):
     """You teleport yourself from your current location to any other spot within range.
      You arrive at exactly the spot desired. It can be a place you can see, one you 
     can visualize, or one you can describe by stating distance and direction, such 
-    as "200 feet straight downward" or "upward to the northwest at a 45-degree 
-    angle, 300 feet". 
+    as "200 feet straight downward" or "upward to the northwest at a 45-degree
+    angle, 300 feet".
     
     You can bring along objects as long as their weight doesn’t
      exceed what you can carry. You can also bring one willing creature of your size
